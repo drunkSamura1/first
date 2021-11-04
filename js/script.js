ae209485cc38c61fe22c639eaec37346d98ef635
Jeka_first <jk.onegin@gmail.com>
2021-11-04 23:37:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// alert ('Hello!');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you 18 y.o.?", "18");
+// console.log(answer);
+
+// const answers = [];
+// answers [0] = prompt('What your first name?', '');
+// answers [1] = prompt('What your second name?', '');
+// answers [2] = prompt('How old you?', '');
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(` hello, ${user}`);
+
+
+// console.log('arr' + " - object");
+// console.log(4 + '5');
+
+
+// let incr = 10,
+// 	decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+
+// const isChecked = true,
+// 	  isClosed = false;
+
+// console.log(isChecked || isClosed); 
